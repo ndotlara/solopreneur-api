0cd114ba17d7dd16a5d828eed71f9f0a0b2ddd5d
ndotlara <natalialara.cd@gmail.com>
2017-10-03 23:33:57 -0400

    All auth scripts are working
    - working through update/PATCH user script, which is not working

--- scripts/members/create-member.sh
+++ scripts/members/create-member.sh
@@ -2,7 +2,7 @@
 
 API="${API_ORIGIN:-http://localhost:4741}"
 URL_PATH="/users"
-curl "${API}${URL_PATH}" \
+curl "${API}${URL_PATH}/${ID}" \
   --include \
   --request POST \
   --header "Content-Type: application/json" \
